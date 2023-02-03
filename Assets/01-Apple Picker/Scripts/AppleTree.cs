9406b8184c12f83bcb8001bb994f6f39e01bf20b
cab557 <102686043+cab557@users.noreply.github.com>
2023-02-02 20:12:32 -0500

    Completed AppleTree Script
    Tweaked speed of tree

--- Assets/01-Apple Picker/Scripts/AppleTree.cs
+++ Assets/01-Apple Picker/Scripts/AppleTree.cs
@@ -6,13 +6,13 @@ public class AppleTree : MonoBehaviour
 {
     public GameObject applePrefab;
 
-    public float speed =1f;
+    public float speed = 1f;
 
-    public float leftandRightEdge = 10f;
+    public float leftAndRightEdge = 10f;
 
-    public float chanceToChangeDirection
+    public float chanceToChangeDirections;
     
-    public float secondsBetweenAppleDrop
+    public float secondsBetweenAppleDrops;
 
    
     // Start is called before the first frame update
@@ -25,10 +25,26 @@ public class AppleTree : MonoBehaviour
     void Update()
     {
         // Basic Movement
-         Vector3 pos = transform.position;      
+        Vector3 pos = transform.position;      
  
-         pos.x += speed * Time.deltaTime;       
+        pos.x += speed * Time.deltaTime;       
  
-         transform.position = pos; 
+        transform.position = pos; 
+
+        //Changing Direction
+         if ( pos.x < -leftAndRightEdge ) {      
+ 
+           speed = Mathf.Abs(speed); // Move ri
+       } else if ( pos.x > leftAndRightEdge ) {
+ 
+           speed = -Mathf.Abs(speed); // Move l
+
     }
+}
+    void FixedUpdate() {
+        // Changing Direction Randomly is now t
+        if ( Random.value < chanceToChangeDirections)
+            speed *= -1; // Change direction
+
+}
 }
