39f7b960b55a71476b2921387d231418139b35fb
cab557 <102686043+cab557@users.noreply.github.com>
2023-02-06 18:30:06 -0500

    Created FollowCam Script

--- Assets/02-Mission Demolition/Scripts/Slingshot.cs
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -66,6 +66,7 @@ public class Slingshot : MonoBehaviour
              projRB.isKinematic = false;
              projRB.collisionDetectionMode = CollisionDetectionMode.Continuous;
              projRB.velocity = -mouseDelta * velocityMult;
+             FollowCam.POI = projectile;
              projectile = null;
         }
 
