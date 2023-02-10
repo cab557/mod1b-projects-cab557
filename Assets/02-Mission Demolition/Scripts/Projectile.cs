030e8a066dc44109670a6c7b44e113db87890946
cab557 <102686043+cab557@users.noreply.github.com>
2023-02-10 00:41:07 -0500

    Created Mission Demolition Script
    Keep track of levels

--- Assets/02-Mission Demolition/Scripts/Projectile.cs
+++ Assets/02-Mission Demolition/Scripts/Projectile.cs
@@ -1,17 +1,17 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent( typeof(Rigidbody))]
+[RequireComponent(typeof(Rigidbody))]
 public class Projectile : MonoBehaviour
-{  
+{
     const int LOOKBACK_COUNT = 10;
-
+    static List<Projectile> PROJECTILES = new List<Projectile>();
     [SerializeField]
     private bool _awake = true;
-    public bool awake {
-        get {return _awake;}
-        private set {_awake = value;}
+    public bool awake
+    {
+        get { return _awake; }
+        private set { _awake = value; }
     }
 
     private Vector3 prevPos;
@@ -24,24 +24,26 @@ public class Projectile : MonoBehaviour
     {
         rigid = GetComponent<Rigidbody>();
         awake = true;
-        prevPos = new Vector3(1000,1000,0);
+        prevPos = new Vector3(1000, 1000, 0);
         deltas.Add(1000);
+        PROJECTILES.Add(this);
 
     }
-    void FixedUpdate() {
+    void FixedUpdate()
+    {
         if (rigid.isKinematic || !awake) return;
 
         Vector3 deltaV3 = transform.position - prevPos;
-        deltas.Add( deltaV3.magnitude);
+        deltas.Add(deltaV3.magnitude);
         prevPos = transform.position;
 
-        while (deltas.Count > LOOKBACK_COUNT) 
+        while (deltas.Count > LOOKBACK_COUNT)
         {
             deltas.RemoveAt(0);
 
-        } 
+        }
         float maxDelta = 0;
-        foreach (float f in deltas) 
+        foreach (float f in deltas)
         {
             if (maxDelta <= Physics.sleepThreshold)
             {
@@ -51,5 +53,20 @@ public class Projectile : MonoBehaviour
         }
     }
 
+    private void OnDestroy()
+    {
+        PROJECTILES.Remove(this);                                          // c
+    }
+
+    static public void DESTROY_PROJECTILES()
+    {                               // d
+        foreach (Projectile p in PROJECTILES)
+        {
+
+            Destroy(p.gameObject);
+        }
+    }
 
 }
+
+
--- Assets/02-Mission Demolition/Scripts/Slingshot.cs
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -68,6 +68,7 @@ public class Slingshot : MonoBehaviour
              projRB.velocity = -mouseDelta * velocityMult;
              FollowCam.POI = projectile;
              projectile = null;
+             MissionDemolition.SHOT_FIRED();
         }
 
 
