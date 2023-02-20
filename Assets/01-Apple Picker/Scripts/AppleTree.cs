b30461d6128cd9fee1e9659d299beb0c9f2fe4a7
cab557 <102686043+cab557@users.noreply.github.com>
2023-02-20 17:13:40 -0500

    Fixed scripts to include Balls Prefab
    fixed camera and positioning

--- Assets/01-Apple Picker/Scripts/AppleTree.cs
+++ Assets/01-Apple Picker/Scripts/AppleTree.cs
@@ -2,55 +2,62 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Balls; 
-public class AppleTree : MonoBehaviour
+namespace Balls
 {
-    public GameObject BallsPrefab;
-
-    public float speed = 1f;
-
-    public float leftAndRightEdge = 10f;
-
-    public float chanceToChangeDirections;
-    
-    public float secondsBetweenBallsDrops;
-
-   
-    // Start is called before the first frame update
-    void Start()
-    {     
-          Invoke( "DropBalls", 2f ); 
-    }
-    void DropBalls() {                         
-        var Balls = Instantiate(BallsPrefab);
-        Balls.transform.position = transform.position;
-        Invoke( "DropBalls", secondsBetweenBallsDrops);
-
-    }
-    // Update is called once per frame
-    void Update()
+    public class AppleTree : MonoBehaviour
     {
-        // Basic Movement
-        Vector3 pos = transform.position;      
- 
-        pos.x += speed * Time.deltaTime;       
- 
-        transform.position = pos; 
+        public GameObject BallsPrefab;
 
-        //Changing Direction
-         if ( pos.x < -leftAndRightEdge ) {      
- 
-           speed = Mathf.Abs(speed); // Move ri
-       } else if ( pos.x > leftAndRightEdge ) {
- 
-           speed = -Mathf.Abs(speed); // Move l
+        public float speed = 1f;
 
+        public float leftAndRightEdge = 10f;
+
+        public float chanceToChangeDirections;
+
+        public float secondsBetweenBallsDrops;
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Invoke("DropBalls", 2f);
+        }
+        void DropBalls()
+        {
+            var Balls = Instantiate(BallsPrefab);
+            Balls.transform.position = transform.position;
+            Invoke("DropBalls", secondsBetweenBallsDrops);
+
+        }
+        // Update is called once per frame
+        void Update()
+        {
+            // Basic Movement
+            Vector3 pos = transform.position;
+
+            pos.x += speed * Time.deltaTime;
+
+            transform.position = pos;
+
+            //Changing Direction
+            if (pos.x < -leftAndRightEdge)
+            {
+
+                speed = Mathf.Abs(speed); // Move ri
+            }
+            else if (pos.x > leftAndRightEdge)
+            {
+
+                speed = -Mathf.Abs(speed); // Move l
+
+            }
+        }
+        void FixedUpdate()
+        {
+            // Changing Direction Randomly is now t
+            if (Random.value < chanceToChangeDirections)
+                speed *= -1; // Change direction
+
+        }
     }
-}
-    void FixedUpdate() {
-        // Changing Direction Randomly is now t
-        if ( Random.value < chanceToChangeDirections)
-            speed *= -1; // Change direction
-
-}
 }
--- Assets/01-Apple Picker/Scripts/apple.cs
+++ Assets/01-Apple Picker/Scripts/apple.cs
@@ -2,28 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Balls;
+namespace Balls { 
 
-    
 
-public class apple : MonoBehaviour
-{
-    public static float     bottomY = -20f; 
-    // Start is called before the first frame update
-    void Start()
+
+    public class apple : MonoBehaviour
     {
-        
-    }
+        public static float bottomY = -20f;
+        // Start is called before the first frame update
+        void Start()
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-          if ( transform.position.y < bottomY ) {
-             Destroy( this.gameObject );  
+        }
 
-             ApplePicker apScript = Camera.main.GetComponent<ApplePicker>();           // b
-             // Call the public AppleMissed() method of apScript
-             apScript.AppleMissed();
+        // Update is called once per frame
+        void Update()
+        {
+            if (transform.position.y < bottomY)
+            {
+                Destroy(this.gameObject);
+
+                ApplePicker apScript = Camera.main.GetComponent<ApplePicker>();           // b
+                                                                                          // Call the public AppleMissed() method of apScript
+                apScript.AppleMissed();
+            }
+        }
     }
-}
 }
